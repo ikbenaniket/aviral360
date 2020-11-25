5e6c536c5a81d850ce4ae67ba61d783a20d1c8e4
ganesh <ganesh706870@gmail.com>
2020-11-25 21:31:15 +0530

    udgrading

--- admin/pages/examples/php/register.php
+++ admin/pages/examples/php/register.php
@@ -31,7 +31,7 @@ if(isset($_POST['login']))
 $password=$_POST['password'];
 $dec_password=$password;
 $useremail=$_POST['uemail'];
-$ret= mysqli_query($con,"SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
+$ret= mysqli_query($conn,"SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
 $num=mysqli_fetch_array($ret);
 if($num>0)
 {
